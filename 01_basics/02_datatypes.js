e1a26aa5e3a896706062053bacf1b3e7dcdc4ad9
SAGAR THAPA <94503760+numb00@users.noreply.github.com>
2024-02-25 14:08:23 +0000

    Data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"  //treat all js code as never version
+
+// alert{3+3} //we are useing nodejs,not browsers 
+console.log(3+3)
+console.log("Sagar")
+
+//Data types
+let name= "sagar"
+let age = 18 
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null = standalone Value
+// undefine =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); //type:-undefine
+console.log(typeof null); //type:-object
