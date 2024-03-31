aa4512fe1f53af2a5c98624545f33d8409cfc3e0
SAGAR THAPA <94503760+numb00@users.noreply.github.com>
2024-03-31 07:51:15 +0000

    Scopes in JS

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,14 +1,60 @@
 // var c = 300
-let a = 300
-if(true){
-    let a =10
-    const b =20
-    console.log("INNER:",a)
-}
-for (let i=0; i < array.length; i++){
-    const element = array[i];
-}
-[]
-console.log(a);
+// let a = 300
+// if(true){
+//     let a =10
+//     const b =20
+//     console.log("INNER:",a)
+// }
+// for (let i=0; i < array.length; i++){
+//     const element = array[i];
+// }
+// []
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+// Nested Scope 
+// function one(){
+    // const username  = "sagar"
+
+    // function two (){
+        // const website = "youtube"
+        // console.log(username);
+    // }
+
+    // console.log(website);
+    // two()
+// }
+
+
+// one()
+
+if (true){
+    const username = "sagar"
+    if(username === "sagar"){
+        const website = "youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+
+
+// +++++++++++++++++++++++++++++  intresting +++++++++++++++++++++++
+
+
+addone(5)
+console.log(addone(5));
+function addone(num){
+    return num + 1
+}
+
+
+console.log(addTwo(5));
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(5)
